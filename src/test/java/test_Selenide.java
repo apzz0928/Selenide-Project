d3375a38fb87f8059fabd3e3ee68358450a11ba0
apzz0928 <apzz0928@gmail.com>
2017-10-17 16:35:15 +0900

    2017/10/17 : IE 적용 테스트 중, text입력 느림 현상 해결중, 64bit

--- src/test/java/test_Selenide.java
+++ src/test/java/test_Selenide.java
@@ -4,6 +4,7 @@ import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
@@ -46,6 +47,14 @@ public class test_Selenide {
 	        RemoteWebDriver driver = new RemoteWebDriver(new URL(urlToRemoteWD),cap);
 	        WebDriverRunner.setWebDriver(driver);
 	  		driver.manage().window().setSize(new Dimension(1600, 1400));
+  		} else if(browser.equals("internetExplorer")) {
+  			TestBrowser = "internetExplorer";
+  			cap = DesiredCapabilities.internetExplorer();
+  			cap.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,true);
+  			cap.setCapability(InternetExplorerDriver.NATIVE_EVENTS, false);
+	        RemoteWebDriver driver = new RemoteWebDriver(new URL(urlToRemoteWD),cap);
+	        WebDriverRunner.setWebDriver(driver);
+	  		driver.manage().window().setSize(new Dimension(1600, 1400));
   		} 
     }
 	private static void js(String javaScriptSource) {
@@ -64,22 +73,25 @@ public class test_Selenide {
 			} 
 		} 
 	}
-	//@Test(priority = 0)
+	@Test(priority = 0)
 	public void alertSample() {
         open(baseUrl);
         js("");
         $(".uid_login_login").waitUntil(appear, 5000);
-        $(".uid_cookie_checkbox").click();
-        $(".uid_test_btn_ok").click();
+        //$(".uid_cookie_checkbox").click();
+        //$(".uid_test_btn_ok").click();
         $(".uid_login_login").click();
         confirm("아이디를 입력해주세요");
-        $(".uid_login_id").setValue("apzz0928");
+        //$(".uid_login_id").setValue("apzz0928");
+        js("$('.uid_login_id').prop('value', 'apzz0928')");
         $(".uid_login_login").click();
         confirm("비밀번호를 입력해주세요.");
-        $(".uid_login_password").setValue("qordlf13");
+        //$(".uid_login_password").setValue("qordlf13");
+        js("$('.uid_login_password').prop('value', 'qordlf13')");
         $(".uid_login_login").click();
         confirm("아이디 또는 비밀번호가 일치하지 않습니다.");
-        $(".uid_login_password").setValue("qordlf12");
+//        $(".uid_login_password").setValue("qordlf12");
+        js("$('.uid_login_password').prop('value', 'qordlf12')");
         $(".uid_login_login").click();
         $(".btn-logout").click();
     }
@@ -108,11 +120,11 @@ public class test_Selenide {
         //System.out.println(source());
         System.out.println("getPageUrlTitleSource --- end ---");
     }
-	@Test(priority = 3)
+	//@Test(priority = 3)
 	public void promptTest() throws InterruptedException {
         open("file:///C:/Users/Administrator/Desktop/Untitled-1.html");
         $(".prompt").click();
-        prompt("값을 입력해주세요", "1234");
+        prompt("값을 입력해주세요", "1234"); // 4.7에서 prompt(); 메소드 추가됨
         Thread.sleep(5000);
     }
 	//@Test(priority = 99)
